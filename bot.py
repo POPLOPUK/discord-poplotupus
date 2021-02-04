358ce4bb600588227860aaf28db1a79239ca550e
chira <pachack223@gmail.com>
2021-02-04 23:43:10 +0000

    new commit

--- bot.py
+++ bot.py
@@ -42,7 +42,8 @@ random_quotes = ["{0} was educated by stray dogs",
                  "{0} looks like a chocolate :D",
                  "{0} is looking like a snack today yum",
                  "{0} have a nice day o/",
-                 "Me: Knock knock\n{0}: Who's there\nMe: Where when\n{0}: Where when who\nMe: My place, tomorrow, you and me ;)",
+                 "Me: Knock knock\n{0}: Who's there\nMe: Where when\n{0}: Where when who\nMe: My place, tomorrow, "
+                 "you and me ;)",
                  "{0}. you looking like a garlic bread today",
                  "You are my best friend {0} UwU",
                  "Wana go out on a date {0}?"]
@@ -83,27 +84,27 @@ async def custom_cmds(message):
         return
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
-    messageL = message.content.lower()
+    message_lower = message.content.lower()
     if not message.channel.id == 577213129080045568:
         print(current_time, str("#") + str(bot.get_channel(message.channel.id)),
               str(message.author)[:-5] + ": " + str(message.content), )
-    if messageL == ".bestgamer":
+    if message_lower == ".bestgamer":
         await message.channel.send("May")
-    elif messageL == ".bestmlg":
+    elif message_lower == ".bestmlg":
         await message.channel.send("Berk")
         await message.channel.send("https://tenor.com/view/pepe-the-frog-dance-happy-meme-pixel-gif-17428498")
-    elif messageL == ".julia":
+    elif message_lower == ".julia":
         await message.channel.send("<:AYAYA:770095211593465886> AYAYA <:AYAYA:770095211593465886>")
-    elif messageL == ".khavan":
+    elif message_lower == ".khavan":
         await message.channel.send('Khavan probably: \"Jay is gay\"')
-    elif messageL == "uwu" or messageL == "owo":
+    elif message_lower == "uwu" or message_lower == "owo":
         emoji = bot.get_emoji(609085517857816577)
         await message.add_reaction(emoji)
-    elif "jay gay" in messageL:
+    elif "jay gay" in message_lower:
         emoji = bot.get_emoji(609086025968648213)
         await message.add_reaction(emoji)
         await message.channel.send(str(message.author)[:-5] + " is gay")
-    elif messageL == "never one":
+    elif message_lower == "never one":
         await message.channel.send("Without the other")
     # below is shoddy code. look away
     probability = getProbability()
@@ -128,7 +129,7 @@ async def reboot(ctx):
     await ctx.bot.logout()
     subprocess.call(["python3", "bot.py"])
 
-
+chance = 0
 resetProbability()
 bot.load_extension("cogs.drawing")
 bot.load_extension("cogs.quotes")
